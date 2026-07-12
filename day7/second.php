4c54e0cbde71908bbf75dfdf8d0344264f46f197
Bhavigupta2710 <bhavigupta2706@gmail.com>
2026-07-12 22:40:52 +0530

    Day7 Assignment

---
+++ day7/second.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>
+    
+    <?php
+    echo "this is my page main title" ?>
+</h1>
+<p>this is my paragraph</p>
+<h2><?php
+    echo "this is my page sub title" ?></h2>
+    <h3><?php //phpinfo(); ?></h3>
+    <?php
+    $name="Bhavi";
+    ?>
+    <h3><?php echo $name; ?></h3>
+</body>
+</html>
